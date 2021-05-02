bb0c290d6488766dd55ca1acdd3636a707c82dcf
minzhoudu <paja98100@gmail.com>
2021-05-03 01:23:34 +0200

    adding logo, input and button.. and styling them

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Tilt from "react-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt
         className="Tilt br2 shadow-2"
-        options={{ max: 25 }}
+        options={{ max: 55 }}
         style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner pa3">
+          <img style={{ paddingLeft: "3px" }} src={brain} alt="brain" />
+        </div>
       </Tilt>
     </div>
   );
