16877e8bc1c59d755018062a2d5c807445a4ee45
minzhoudu <paja98100@gmail.com>
2021-05-03 00:31:34 +0200

    folder structure and project planning

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/* <ImageLinkForm />
+      <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
