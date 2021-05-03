e8f178ac96e75586f9fe22b2834db9ec4d7d8185
minzhoudu <paja98100@gmail.com>
2021-05-03 02:42:56 +0200

    added rank link and navigation components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
 import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
