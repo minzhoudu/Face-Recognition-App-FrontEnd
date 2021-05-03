24da52a49ea029223e021e2f3d21b160eb08b7f3
minzhoudu <paja98100@gmail.com>
2021-05-03 14:44:32 +0200

    adding some minor changes

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -4,7 +4,7 @@ const FaceRecognition = ({ imageUrl }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={imageUrl} alt="face" width="500px" height="auto" />
+        <img src={imageUrl} alt="" width="500px" height="auto" />
       </div>
     </div>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,6 +4,7 @@ const Navigation = () => {
   return (
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
       <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+      {/* <button className="f3 dim black pa3 pointer ">Sign out</button> */}
     </nav>
   );
 };
