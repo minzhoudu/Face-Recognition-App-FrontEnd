0b396bc77b14044d1c9a3f604f1306697237d364
minzhoudu <paja98100@gmail.com>
2021-05-03 13:43:04 +0200

    adding and setting up Clarifai api for image face recognition

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import "./App.css";
 
 const particlesOptions = {
@@ -18,7 +20,14 @@ const particlesOptions = {
   },
 };
 
+const app = new Clarifai.App({
+  apiKey: "bc28748779ad4a3c8aa525dce4efb43e",
+});
+
 class App extends Component {
+  state = {
+    input: "",
+  };
   render() {
     return (
       <div className="App">
@@ -26,11 +35,34 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onSubmit={this.onBtnSubmit}
+          onInputChange={this.onChangeHandler}
+        />
+        <FaceRecognition />
       </div>
     );
   }
+  //handlers
+  onChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  onBtnSubmit = () => {
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          //code
+        }
+      );
+  };
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className="f3">
         {"This Magic Brain will detect faces in your pictures. Give it a try!"}
       </p>
       <div className="center form pa4 br3 shadow-5">
-        <input className="f4 pa2 w-70 center" type="text" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+        <input
+          className="f4 pa2 w-70 center"
+          type="text"
+          onChange={onInputChange}
+        />
+        <button
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          onClick={onSubmit}
+        >
           Detect
         </button>
       </div>
