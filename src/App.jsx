65ff59b81e3d2b1263af90c38b22f655919107eb
Pavle <paja98100@gmail.com>
2021-05-14 19:54:12 +0200

    face detection box added for one face in the image

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Particles from "react-particles-js";
 import Clarifai from "clarifai";
 import "./App.css";
 
+//particles options
 const particlesOptions = {
   particles: {
     number: {
@@ -20,14 +21,17 @@ const particlesOptions = {
   },
 };
 
+//Clarify api
 const app = new Clarifai.App({
   apiKey: "bc28748779ad4a3c8aa525dce4efb43e",
 });
 
+//APP
 class App extends Component {
   state = {
     input: "",
     imageUrl: "",
+    box: {},
   };
   render() {
     return (
@@ -40,7 +44,7 @@ class App extends Component {
           onSubmit={this.onBtnSubmit}
           onInputChange={this.onChangeHandler}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
@@ -51,16 +55,35 @@ class App extends Component {
 
   onBtnSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {
-        //code
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBoxHandler(this.calculateFaceLocationHandler(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  calculateFaceLocationHandler = (data) => {
+    const clarifaiFaceData =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById("inputImage");
+
+    const imgWidth = Number(image.width);
+    const imgHeight = Number(image.height);
+    console.log(imgWidth, imgHeight);
+
+    return {
+      leftCol: clarifaiFaceData.left_col * imgWidth,
+      topRow: clarifaiFaceData.top_row * imgHeight,
+      rightCol: imgWidth - clarifaiFaceData.right_col * imgWidth,
+      bottomRow: imgHeight - clarifaiFaceData.bottom_row * imgHeight,
+    };
+  };
+
+  displayFaceBoxHandler = (box) => {
+    console.log(box);
+    this.setState({ box: box });
   };
 }
 
