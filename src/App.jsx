9fb8be2c9223b05119b5c7f86b4163089a54baf1
Pavle <paja98100@gmail.com>
2021-05-14 21:08:10 +0200

    front end part finished.. added sign in and register forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+//components
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
+//3rd party
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
 import "./App.css";
@@ -32,19 +36,35 @@ class App extends Component {
     input: "",
     imageUrl: "",
     box: {},
+    route: "signin",
+    isSignedIn: false,
   };
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onSubmit={this.onBtnSubmit}
-          onInputChange={this.onChangeHandler}
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        {this.state.route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onSubmit={this.onBtnSubmit}
+              onInputChange={this.onChangeHandler}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
@@ -66,12 +86,9 @@ class App extends Component {
   calculateFaceLocationHandler = (data) => {
     const clarifaiFaceData =
       data.outputs[0].data.regions[0].region_info.bounding_box;
-
     const image = document.getElementById("inputImage");
-
     const imgWidth = Number(image.width);
     const imgHeight = Number(image.height);
-    console.log(imgWidth, imgHeight);
 
     return {
       leftCol: clarifaiFaceData.left_col * imgWidth,
@@ -82,8 +99,16 @@ class App extends Component {
   };
 
   displayFaceBoxHandler = (box) => {
-    console.log(box);
     this.setState({ box: box });
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
   };
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 
-const Navigation = () => {
-  return (
-    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
-      {/* <button className="f3 dim black pa3 pointer ">Sign out</button> */}
-    </nav>
-  );
+const Navigation = (props) => {
+  if (props.isSignedIn) {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => props.onRouteChange("signout")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => props.onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+
+        <p
+          onClick={() => props.onRouteChange("register")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Navigation;
